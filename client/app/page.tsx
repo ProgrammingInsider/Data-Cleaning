91709cfc1ac58f4955c1f27a95e9e64ee1c425ac
Amanuel Abera <amanuelabera46@gmail.com>
2025-01-03 01:51:10 +0300

    Client Updated

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,9 @@
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex flex-col gap-6 justify-center items-center min-h-screen">
       <h1 className="text-5xl font-bold">Home page is on going...</h1>
+      <Link href={'/projects'} className="primaryBtn">Go to Project Dashboard page</Link>
     </div>
   );
 }
