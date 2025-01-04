06810666c281ff3b5fc25f908eba7f669c2e3e37
Amanuel Abera <amanuelabera46@gmail.com>
2025-01-04 22:44:17 +0300

    Configured for AWS

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -1,9 +1,10 @@
-export default {
+  module.exports = {
     apps: [
       {
         name: "data-cleaning-server",
         script: "npm",
-        args: "run start",
+        args: "run start", 
+        cwd: "/root/Data-Cleaning/server", 
         env: {
           NODE_ENV: "production",
           PORT: 80,
