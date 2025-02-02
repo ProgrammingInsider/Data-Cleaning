ac563d4d57a645dcc2a7cf8a241eecf7366ace7d
Amanuel Abera <amanuelabera46@gmail.com>
2025-02-03 01:12:39 +0300

    The clean data workstation first phase I completed

--- client/app/cleandata/[fileid]/layout.tsx
+++ client/app/cleandata/[fileid]/layout.tsx
@@ -19,15 +19,16 @@ export default async function CleanDataLayout({
         <div className="w-screen h-[calc(100vh-70px)] flex border-t border-b border-gray-800">
             <ResizablePanelGroup direction="horizontal" className="w-full h-full">
                 <ResizablePanel defaultSize={15} className="border-r border-gray-800">
-                    <Suspense fallback={<p>Loading filed...</p>}>{sidemenu}</Suspense>
+                    <Suspense>{sidemenu}</Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
                 <ResizablePanel defaultSize={20} className="border-r border-gray-800">
-                    <Suspense fallback={<p>Loading chat...</p>}>{chat}</Suspense>
+                    <Suspense>{chat}</Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
                 <ResizablePanel defaultSize={65}>
-                    <Suspense fallback={<p>Loading table...</p>}>{table}</Suspense>
+                    <Suspense>{table}</Suspense>
+                    {/* <Suspense fallback={<p>Loading table...</p>}>{table}</Suspense> */}
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
