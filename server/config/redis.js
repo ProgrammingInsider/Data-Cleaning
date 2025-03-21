64f48098012f3a659187515d292d46c54dc522a2
Amanuel Abera <amanuelabera46@gmail.com>
2025-03-21 09:45:14 +0300

    Redis cache the dataset

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,10 +1,23 @@
+// import Redis from 'ioredis';
+
+// const client = new Redis({
+//   url: `rediss://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+//   tls: {
+//       rejectUnauthorized: false 
+//   }
+// });
+
+// export default client;
+
+
 import Redis from 'ioredis';
 
 const client = new Redis({
-  url: `rediss://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  host: process.env.REDIS_HOST,  
+  port: process.env.REDIS_PORT,
   tls: {
-      rejectUnauthorized: false 
-  }
+    rejectUnauthorized: false, 
+  },
 });
 
 export default client;
