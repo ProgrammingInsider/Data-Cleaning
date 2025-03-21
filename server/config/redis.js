0ea31cfd59837d562c403c965a27abcc47823c86
Amanuel Abera <amanuelabera46@gmail.com>
2025-03-21 09:12:15 +0300

    Redis cache the dataset

--- server/config/redis.js
+++ server/config/redis.js
@@ -7,10 +7,4 @@ const client = new Redis({
   }
 });
 
-// Example usage:
-// client.set('foo2', 'bar2');
-// client.get('foo2', (err, result) => {
-//     console.log(result); 
-// });
-
 export default client;
